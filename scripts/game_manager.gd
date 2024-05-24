495aabd98276ee7b969817432a1dc10236efa24f
Oliver Ebsworth <ebswortholiver@gmail.com>
2024-05-24 22:09:43 +0100

    Added a 'coin_counter' scene that attaches to a "CanvasLayer" for the UI.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,7 +3,9 @@ extends Node
 var score = 0
 
 @onready var score_label = $ScoreLabel
+@onready var coin_label = $"../UserInterface/CoinCounter/CoinLabel"
 
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins."
+	coin_label.text = str(score)
