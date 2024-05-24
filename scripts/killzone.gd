1a5d2569df67c9a3d39fc50fab218a6cc87c66b4
Oliver Ebsworth <ebswortholiver@gmail.com>
2024-05-24 17:24:43 +0100

    Added a slow motion effect when the player dies.

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,7 +4,9 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("You died!")
+	Engine.time_scale = 0.5
 	timer.start()
 	
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
